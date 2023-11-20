d15827b427dc6209489c94289eb9a20d1f6cd38b
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2023-11-21 03:13:25 +0800

    open links in new tab

--- src/lib/components/Chip/Chip.svelte
+++ src/lib/components/Chip/Chip.svelte
@@ -7,6 +7,7 @@
 	export let size = 'auto';
 	export let classes = '';
 	export let href = '';
+	export let target: string | undefined = undefined;
 
 	$: className = `row-center cursor-pointer py-[5px] px-[15px] m-[2.5px] decoration-none inline-block border-[1px] border-solid border-[var(--border)] rounded-[20px] tracking-wider text-[0.9em] text-[var(--tertiary-text)] duration-[150ms] font-light  ${
 		active
@@ -22,6 +23,7 @@
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <svelte:element
 	this={href ? 'a' : 'button'}
+	target={target ?? undefined}
 	bind:this={el}
 	{href}
 	class={className}
