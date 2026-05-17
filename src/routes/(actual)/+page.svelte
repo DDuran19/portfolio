1ed1e96fae12736659ce99039221c1f6da336c39
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2026-05-18 01:17:23 +0800

    let TabTitle handle the meta tags

--- src/routes/(actual)/+page.svelte
+++ src/routes/(actual)/+page.svelte
@@ -3,6 +3,7 @@
 	import Carrousel from '$lib/components/Carrousel/Carrousel.svelte';
 	import Icon from '$lib/components/Icon/Icon.svelte';
 	import MainTitle from '$lib/components/MainTitle/MainTitle.svelte';
+	import TabTitle from '$lib/components/TabTitle.svelte';
 	import { TITLE_SUFFIX } from '$lib/params';
 	import { HOME, getPlatfromIcon } from '$lib/params';
 	import MY_SKILLS from '$lib/skills.params';
@@ -90,27 +91,9 @@
 			badge.addEventListener('click', handleClick);
 		});
 	});
-	let origin = $derived(page.url.origin);
-	let fullTitle = $derived(useTitle(title, TITLE_SUFFIX));
 </script>
 
-<svelte:head>
-	<title>{fullTitle}</title>
-	<meta name="description" content={description} />
-	<meta name="author" content="Denver James Duran" />
-	<!-- Open Graph -->
-	<meta property="og:title" content={fullTitle} />
-	<meta property="og:description" content={description} />
-	<meta property="og:image" content="https://denvie.online/og-image.png" />
-	<meta property="og:url" content="https://denvie.online" />
-	<meta property="og:type" content="website" />
-
-	<!-- Twitter/X -->
-	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:title" content={fullTitle} />
-	<meta name="twitter:description" content={description} />
-	<meta name="twitter:image" content="https://denvie.online/og-image.png" />
-</svelte:head>
+<TabTitle {title} {description} />
 <div
 	class="col self-center flex-1 md:flex-row md:slef-stretch justify-center lg:justify-between items-center p-y-0px p-x-10px"
 >
